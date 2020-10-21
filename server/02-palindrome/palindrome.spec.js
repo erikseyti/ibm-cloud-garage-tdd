983117ca84eb46f7d28658fa0e0d4347d08205a6
Erik Seyti <erikseyti@gmail.com>
2020-10-21 20:45:20 -0300

    tests for palindrome.spec.ts

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,5 +1,39 @@
+let isPalindrome = (phrase) => {
+  if (phrase === '') throw new Error('empty strings are not palindromes');
+  if (phrase.trim() === '') return false;
+  return phrase === phrase.split('').reverse().join('')
+};
+
 describe('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
+
+  it('true for mom',() => {
+    expect(isPalindrome('mom')).toBe(true);
+  });
+
+  it('false for dude', () =>{
+    expect(isPalindrome('dude')).toBe(false);
+  });
+
+  it('true for mom mom', () => {
+    expect(isPalindrome('mom mom')).toBe(true);
+  });
+
+  it('false for dad mom', () =>{
+    expect(isPalindrome('dad mom')).toBe(false);
+  });
+
+  it('false for whitespace', () => {
+    expect(isPalindrome('   ')).toBe(false);
+  });
+
+  it('error for not a string', () => {
+    expect(() => {
+      isPalindrome('')
+    }).toThrowError('empty strings are not palindromes');
+  });
+
+
 });
