57c5919e9490bc7ea6462dc3fc516b5ad40390b1
Erik Seyti <erikseyti@gmail.com>
2020-10-23 19:58:05 -0300

    Finish Kata of prime numbers

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -1,3 +1,12 @@
+let findPrimeFactors = (number) => {
+  let factors = [];
+  for (let divisor = 2; number > 1; divisor += 1) {
+    for (; number % divisor === 0; number /= divisor) factors.push(divisor);
+  }
+  return factors;
+};
+
+
 describe('the prime factors canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
@@ -5,13 +14,40 @@ describe('the prime factors canary spec', () => {
 });
 
 describe('a prime factors function should', () => {
-  it.todo('return none for 1');
-  it.todo('return 2 for 2');
-  it.todo('return 3 for 3');
-  it.todo('return 2, 2 for 4');
-  it.todo('return 5 for 5');
-  it.todo('return 2, 3 for 6');
-  it.todo('return 7 for 7');
-  it.todo('return 2, 2, 2 for 8');
-  it.todo('return 3, 3 for 9');
+
+  it('return none for 1', () => {
+    expect(findPrimeFactors(1)).toEqual([]);
+  });
+
+  it('return 2 for 2', () => {
+    expect(findPrimeFactors(2)).toEqual([2]);
+  });
+
+  it('return 3 for 3', () => {
+    expect(findPrimeFactors(3)).toEqual([3]);
+  });
+
+  it('return 2, 2 for 4', () => {
+    expect(findPrimeFactors(4)).toEqual([2, 2]);
+  });
+
+  it('return 5 for 5', () => {
+    expect(findPrimeFactors(5)).toEqual([5]);
+  });
+
+  it('return 2, 3 for 6', () => {
+    expect(findPrimeFactors(6)).toEqual([2, 3]);
+  });
+
+  it('return 7 for 7', () => {
+    expect(findPrimeFactors(7)).toEqual([7]);
+  });
+
+  it('return 2, 2, 2 for 8', () => {
+    expect(findPrimeFactors(8)).toEqual([2, 2, 2]);
+  });
+  
+  it('return 3, 3 for 9', () => {
+    expect(findPrimeFactors(9)).toEqual([3, 3]);
+  });
 });
